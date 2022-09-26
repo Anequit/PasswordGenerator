0041f71bbd6f23a381a439d946579d0e169b5a1c
Bailey <Anequit@gmail.com>
2022-09-25 21:45:44 -0400

    Add benchmark

---
+++ src/PasswordGenerator.Benchmark/Benchmarks.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace PasswordGenerator.Benchmark;
+
+[MemoryDiagnoser(false)]
+public class Benchmarks
+{
+    [Params(10_000, 100_000, 1_000_000, 10_000_000)]
+    public int Length { get; set; }
+
+    [Benchmark]
+    public string GeneratePassword() => Core.Generator.GeneratePassword(Length, true, true);
+}
---
+++ src/PasswordGenerator.Benchmark/Program.cs
@@ -0,0 +1,8 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace PasswordGenerator.Benchmark;
+
+internal class Program
+{
+    static void Main(string[] args) => BenchmarkRunner.Run<Benchmarks>();
+}
