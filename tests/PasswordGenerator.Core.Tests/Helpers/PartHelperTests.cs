68e060220f1be7a119af5429e91b2615be91fc60
Bailey <Anequit@gmail.com>
2023-07-11 14:47:51 -0400

    Update PartHelperTests.cs

--- tests/PasswordGenerator.Core.Tests/Helpers/PartHelperTests.cs
+++ tests/PasswordGenerator.Core.Tests/Helpers/PartHelperTests.cs
@@ -5,9 +5,9 @@ namespace PasswordGenerator.Core.Tests.Helpers;
 
 public class PartHelperTests
 {
-    [TestCase(2130012, 2131)]
-    [TestCase(556565512, 556566)]
-    [TestCase(11212, 12)]
+    [TestCase(2130012, 3)]
+    [TestCase(556565512, 1)]
+    [TestCase(11212, 1)]
     public void PartHelper_CorrectLength(int length, long expected)
     {
         int result = PartHelper.InitializePartArray(length).Length;
