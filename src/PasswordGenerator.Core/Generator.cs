7822b8696164376eea3812928139a7634e5e5d93
Bailey <Anequit@gmail.com>
2023-07-12 21:29:30 -0400

    Refactor Generator.cs

--- src/PasswordGenerator.Core/Generator.cs
+++ src/PasswordGenerator.Core/Generator.cs
@@ -1,7 +1,6 @@
 ﻿using PasswordGenerator.Core.Helpers;
 using System;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace PasswordGenerator.Core;
 
@@ -9,27 +8,26 @@ public static class Generator
 {
     public static string GeneratePassword(in int length, in bool symbols, in bool numbers)
     {
-        if(length > 100000000)
+        if(length > 1_000_000_000)
             return "Length too long";
 
-        char[] pool = BuildPool(symbols, numbers);
+        ReadOnlySpan<char> pool = BuildPool(symbols, numbers);
 
         int[] partArray = PartHelper.InitializePartArray(length);
 
         StringBuilder builder = new StringBuilder(length);
 
-        Parallel.ForEach(partArray, part =>
+        foreach (int partLength in partArray)
         {
-            lock(builder)
-                builder.Append(GeneratePartData(part, pool));
-        });
-
+            GeneratePartData(builder, partLength, pool);
+        }
+        
         GC.Collect(); // Cleanup any allocated string builders from GeneratePartData
 
         return builder.ToString();
     }
 
-    private static char[] BuildPool(in bool symbols, in bool numbers)
+    private static ReadOnlySpan<char> BuildPool(in bool symbols, in bool numbers)
     {
         string pool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
@@ -39,18 +37,14 @@ public static class Generator
         if (numbers)
             pool += "1234567890";
 
-        return pool.ToCharArray();
+        return pool.AsSpan();
     }
 
-    private static string GeneratePartData(in int length, in char[] pool)
+    private static void GeneratePartData(in StringBuilder builder, in int length, in ReadOnlySpan<char> pool)
     {
-        StringBuilder builder = new StringBuilder(length);
-
-        do
+        for (int x = 0; x < length; x++)
         {
             builder.Append(pool[Random.Shared.Next(0, pool.Length)]);
-        } while(builder.Length < length);
-
-        return builder.ToString();
+        }
     }
 }
